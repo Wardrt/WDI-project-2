46f33935876cbbd358734a4fc7dccf17f357004a
Wardrt <ryanward1@btinternet.com>
2016-04-22 15:46:23 +0100

    Nested the episodes in the podcasts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   resources :users, only: :show
-  resources :podcasts, only: [:index, :show]
+  resources :podcasts, only: [:index, :show] do
+    resources :episodes, only: [:index, :show]
+  end
 
   root "statics#home"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p1 = Podcast.create!(title: "Test",
+                    description: "This is a test.",
+                    image: "http://fillmurray.com/300/300",
+                    genre: "something",
+                    podcaster: "Person")
