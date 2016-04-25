184118d9890a7b84d9aac92112f67c72a2214662
Wardrt <ryanward1@btinternet.com>
2016-04-25 16:31:19 +0100

    Fixed the follows working on the podcast show page.

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160425132036) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "name"
+    t.string   "image"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
@@ -84,7 +85,6 @@ ActiveRecord::Schema.define(version: 20160425132036) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
