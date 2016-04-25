29741fed872e05881b4163395fde13896f467e77
Wardrt <ryanward1@btinternet.com>
2016-04-25 20:02:50 +0100

    Can now see the podcasts without being signed in, but can't follow.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
-p1 = Podcast.create!(title: "Test",
-                    description: "This is a test.",
-                    image: "http://fillmurray.com/300/300",
-                    genre: "something",
-                    podcaster: "Person")
+User.delete_all
+Podcast.delete_all
+Episode.delete_all
 
-e1 = p1.episodes.create!(title: "Test",
-                        description: "This is a test",
-                        guest: "Some Guy",
-                        url: "259962866")
+p1 = Podcast.create!(title: "She Didn't Text Back",
+                    description: "The She Didn't Text Back Podcast showcases Daren VonGirdner and William Haynes discussing everything from relationship issues to instagram baddies. If you desire more- well, go somewhere else.",
+                    image: "/images/didnttextback800.jpg",
+                    genre: "Comedy",
+                    podcaster: "William Haynes and Daren VonGirdner")
+
+e1 = p1.episodes.create!(title: "Does The Friendzone Exist? #1",
+                        description: "Will and Daren introduce the podcast and take some questions from fans asking about the 'friendzone'.",
+                        url: "200681447")
+
+
