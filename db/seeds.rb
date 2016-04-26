e6b5aee2e6cdd60ff96b7375aa9f03b70d125a8f
Wardrt <ryanward1@btinternet.com>
2016-04-26 13:56:27 +0100

    Landing page is loading the latest podcast, and no more overlap on the the 2 divs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,19 @@ e1 = p1.episodes.create!(title: "Does The Friendzone Exist? #1",
                         url: "200681447")
 
 
+
+
+
+
+
+p2 = Podcast.create!(title: "Cloverfeels",
+                    description: "Mike Falzone and Steve Zaragoza love many things together and separately. One of those things happens to be the mystery that surrounds the ‘Cloverfield’ movie franchise. Whether they’re figuring out what really happened to Tagruato Oil Rig in 2008 or debating about why John Goodman looks like he’s been 57 since for thirty years, one thing is for sure – we’re all about to catch some Cloverfeels. Pull your first generation ipod up to 10 Cloverfield Lane. I bet you can’t listen to just 6!",
+                    image: "/images/clover800.jpg",
+                    genre: "Comedy, Film",
+                    podcaster: "Mike Falzone and Steve Zaragoza")
+
+
+
+
+
+
